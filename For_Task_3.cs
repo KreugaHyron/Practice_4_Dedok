9747b79ce9950a2dc9516af8359d52ae5a3a1db8
DXdok_zp32@student.itstep.org <DXdok_zp32@student.itstep.org>
2024-09-20 18:53:54 +0300

    Добавьте файлы проекта.

---
+++ For_Task_3.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_4_Dedok
+{
+    public class Backpack
+    {
+        public string Color { get; set; }
+        public string Manufacturer { get; set; }
+        public double Weight { get; set; }
+        public double Volume { get; set; }
+
+        private Item[] contents;
+        private int itemCount; 
+
+        public event EventHandler<ItemEventArgs> ItemAdded;
+        public event EventHandler<ItemEventArgs> ItemRemoved;
+
+        public Backpack(string color, string manufacturer, double weight, double volume, int capacity)
+        {
+            Color = color;
+            Manufacturer = manufacturer;
+            Weight = weight;
+            Volume = volume;
+            contents = new Item[capacity];
+            itemCount = 0;
+        }
+        public void AddItem(Item item)
+        {
+            if (itemCount >= contents.Length)
+            {
+                Console.WriteLine("Рюкзак переповнений!");
+                return;
+            }
+
+            contents[itemCount] = item;
+            itemCount++;
+            OnItemAdded(new ItemEventArgs(item));
+        }
+        public void RemoveItem(Item item)
+        {
+            int index = Array.IndexOf(contents, item);
+            if (index >= 0)
+            {
+                for (int i = index; i < itemCount - 1; i++)
+                {
+                    contents[i] = contents[i + 1];
+                }
+                contents[itemCount - 1] = null;
+                itemCount--;
+                OnItemRemoved(new ItemEventArgs(item));
+            }
+        }
+        protected virtual void OnItemAdded(ItemEventArgs e)
+        {
+            ItemAdded?.Invoke(this, e);
+        }
+        protected virtual void OnItemRemoved(ItemEventArgs e)
+        {
+            ItemRemoved?.Invoke(this, e);
+        }
+        public Item[] GetContents()
+        {
+            Item[] currentContents = new Item[itemCount];
+            Array.Copy(contents, currentContents, itemCount);
+            return currentContents;
+        }
+    }
+    public class Item
+    {
+        public string Name { get; set; }
+        public double Volume { get; set; }
+
+        public Item(string name, double volume)
+        {
+            Name = name;
+            Volume = volume;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} (Volume: {Volume}L)";
+        }
+    }
+    public class ItemEventArgs : EventArgs
+    {
+        public Item Item { get; private set; }
+
+        public ItemEventArgs(Item item)
+        {
+            Item = item;
+        }
+    }
+    public static class BackpackExtensions
+    {
+        public static bool CanAddItem(this Backpack backpack, Item item)
+        {
+            double usedVolume = 0;
+            foreach (var i in backpack.GetContents())
+            {
+                usedVolume += i.Volume;
+            }
+
+            return usedVolume + item.Volume <= backpack.Volume;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using Practice_4_Dedok;
+public static class StringExtensions
+{
+    public static int SentenceCount(this string text)
+    {
+        if (string.IsNullOrWhiteSpace(text))
+            return 0;
+        char[] sentenceEndings = { '.', '!', '?' };
+        string[] sentences = text.Split(sentenceEndings, StringSplitOptions.RemoveEmptyEntries);
+        return sentences.Length;
+    }
+}
+//2
+public static class StringExtensions_1
+{
+    public static int WordsStartingAndEndingWithSameLetter(this string text_1)
+    {
+        if (string.IsNullOrWhiteSpace(text_1))
+            return 0;
+        string[] words = text_1.Split(new[] { ' ', '\t', '\n', '\r', ',', '.', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+        return words.Count(word => word.Length > 1 &&
+        char.ToLower(word.First()) == char.ToLower(word.Last()));
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        //1
+        Console.WriteLine("Task_1: ");
+        string text = "Привіт! Як твої справи? Це чудовий день.";
+        int count = text.SentenceCount();
+        Console.WriteLine($"Кількість речень: {count}");
+        Console.WriteLine();
+        Console.ReadKey();
+        //2
+        Console.WriteLine("Task_2: ");
+        string text_1 = "Anna has a radar level kayak, but not every word ends the same.";
+        int count_1 = text_1.WordsStartingAndEndingWithSameLetter();
+        Console.WriteLine($"Кількість слів: {count_1}");
+        Console.WriteLine();
+        Console.ReadKey();
+        //3
+        var backpack = new Backpack("Чорний", "Nike", 1.2, 20, 5);
+
+        backpack.ItemAdded += (sender, e) => Console.WriteLine($"Додано: {e.Item}");
+        backpack.ItemRemoved += (sender, e) => Console.WriteLine($"Вилучено: {e.Item}");
+
+        var item1 = new Item("Книга", 3);
+        var item2 = new Item("Ноутбук", 5);
+
+        if (backpack.CanAddItem(item1))
+        {
+            backpack.AddItem(item1);
+        }
+
+        if (backpack.CanAddItem(item2))
+        {
+            backpack.AddItem(item2);
+        }
+
+        backpack.RemoveItem(item1);
+    }
+}
